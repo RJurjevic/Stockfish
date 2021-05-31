f372ebf67538bd291c6db7f9f9c751aa1fc5c498
Robert Jurjevic <76825350+kamenicvijet@users.noreply.github.com>
2021-05-31 20:42:44 +0100

    makefish.sh
    added makefish.sh for build on Windows, etc.

---
+++ makefish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# makefish.sh
+
+# install packages if not already installed
+unzip -v &> /dev/null || pacman -S --noconfirm unzip
+make -v &> /dev/null || pacman -S --noconfirm make
+g++ -v &> /dev/null || pacman -S --noconfirm mingw-w64-x86_64-gcc
+
+cd src
+
+# build Stockfish executable
+make build ARCH=x86-64-sse41-popcnt blas=yes flipped=yes 
+##make build ARCH=x86-64-avx2 blas=yes flipped=yes -j
+strip stockfish.exe
+mv stockfish.exe ../stockfish-sse41-flipped-2021-05-31.exe
+##mv stockfish.exe ../stockfish-avx2-flipped-2021-03-01.exe
+make clean
+cd
