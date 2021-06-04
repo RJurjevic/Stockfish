a5fcd156dbdfd6b21e86c9aa8586de8170405984
Robert Jurjevic <76825350+kamenicvijet@users.noreply.github.com>
2021-06-04 20:18:14 +0100

    avx2 build
    avx2 build for Google Cloud VM training

--- makefish.sh
+++ makefish.sh
@@ -9,10 +9,11 @@ g++ -v &> /dev/null || pacman -S --noconfirm mingw-w64-x86_64-gcc
 cd src
 
 # build Stockfish executable
-make build ARCH=x86-64-sse41-popcnt blas=yes flipped=yes 
+make build ARCH=x86-64-avx2 blas=yes flipped=yes 
+##make build ARCH=x86-64-sse41-popcnt blas=yes flipped=yes 
 ##make build ARCH=x86-64-avx2 blas=yes flipped=yes -j
 strip stockfish.exe
-mv stockfish.exe ../stockfish-sse41-flipped-2021-06-02.exe
-##mv stockfish.exe ../stockfish-avx2-flipped-2021-03-01.exe
+##mv stockfish.exe ../stockfish-sse41-flipped-2021-06-02.exe
+mv stockfish.exe ../stockfish-avx2-flipped-2021-06-04.exe
 make clean
 cd
