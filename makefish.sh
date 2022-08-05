08496ac43507c797ed66ca4cf92a35966e4a22f6
Robert Jurjevic <stone.flower@outlook.com>
2022-08-06 00:28:00 +0100

    rebuild
    just rebuild

--- makefish.sh
+++ makefish.sh
@@ -11,6 +11,6 @@ cd src
 # build Stockfish executable
 make build ARCH=x86-64-avx2 blas=yes
 strip stockfish.exe
-mv stockfish.exe ../stockfish-avx2-halfkp-2022-06-21.exe
+mv stockfish.exe ../stockfish-x86-64-avx2-windows-2022-08-06.exe
 make clean
 cd
